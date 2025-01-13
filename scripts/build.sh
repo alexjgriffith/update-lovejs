f15068cbbe188c699e8ca48b53e31ad102d04403
alexjgriffith <griffitaj@gmail.com>
2025-01-12 21:26:16 -0500

    setup branch to implement lua5.4 for lovejs

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,17 +7,17 @@ mkdir -p build/release build/release-single build/compat build/compat-single
 
 source ${EMSDK}/emsdk_env.sh
 
-(
-  cd build/release
-  emcmake cmake ${MEGASOURCE} -DLOVE_JIT=0 -DCMAKE_BUILD_TYPE=Release -DCMAKE_POSITION_INDEPENDENT_CODE=ON
-  emmake make -j 8
-)
+# (
+#   cd build/release
+#   emcmake cmake ${MEGASOURCE} -DLOVE_JIT=0 -DCMAKE_BUILD_TYPE=Release -DCMAKE_POSITION_INDEPENDENT_CODE=ON
+#   emmake make -j 8
+# )
 
-(
-  cd build/release-single
-  emcmake cmake ${MEGASOURCE} -DLOVE_JIT=0 -DCMAKE_BUILD_TYPE=Release -DCMAKE_POSITION_INDEPENDENT_CODE=ON -DLOVEJS_SINGLE=ON
-  emmake make -j 8 
-)
+# (
+#   cd build/release-single
+#   emcmake cmake ${MEGASOURCE} -DLOVE_JIT=0 -DCMAKE_BUILD_TYPE=Release -DCMAKE_POSITION_INDEPENDENT_CODE=ON -DLOVEJS_SINGLE=ON
+#   emmake make -j 8 
+# )
 
 (
   cd build/compat
@@ -25,8 +25,8 @@ source ${EMSDK}/emsdk_env.sh
   emmake make -j 8
 ) 
 
-(
-  cd build/compat-single
-  emcmake cmake ${MEGASOURCE} -DLOVE_JIT=0 -DCMAKE_BUILD_TYPE=Release -DLOVEJS_COMPAT=1 -DSEXPORT_ALL=1 -DSMAIN_MODULE=1 -DSERROR_ON_UNDEFINED_SYMBOLS=0 -DCMAKE_POSITION_INDEPENDENT_CODE=ON -DLOVEJS_SINGLE=ON
-  emmake make -j 8
-) 
+# (
+#   cd build/compat-single
+#   emcmake cmake ${MEGASOURCE} -DLOVE_JIT=0 -DCMAKE_BUILD_TYPE=Release -DLOVEJS_COMPAT=1 -DSEXPORT_ALL=1 -DSMAIN_MODULE=1 -DSERROR_ON_UNDEFINED_SYMBOLS=0 -DCMAKE_POSITION_INDEPENDENT_CODE=ON -DLOVEJS_SINGLE=ON
+#   emmake make -j 8
+# ) 
