e89c983e83496885ca86b7a14f3feec694ba6b62
alexjgriffith <griffitaj@gmail.com>
2025-01-14 18:18:01 -0500

    Fixed Lua5.4 build

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,30 +3,66 @@
 MEGASOURCE=$1
 EMSDK=$2
 
-mkdir -p build/release build/release-single build/compat build/compat-single
+base=0
+lua54=1
 
 source ${EMSDK}/emsdk_env.sh
 
-# (
-#   cd build/release
-#   emcmake cmake ${MEGASOURCE} -DLOVE_JIT=0 -DCMAKE_BUILD_TYPE=Release -DCMAKE_POSITION_INDEPENDENT_CODE=ON
-#   emmake make -j 8
-# )
+if [[ $base = 1 ]]
+then
+    mkdir -p build/release build/release-single build/compat build/compat-single
+    (
+        cd build/release
+        emcmake cmake ${MEGASOURCE} -DLOVE_JIT=0 -DCMAKE_BUILD_TYPE=Release -DCMAKE_POSITION_INDEPENDENT_CODE=ON
+        emmake make -j 8
+    )
+    
+    (
+        cd build/release-single
+        emcmake cmake ${MEGASOURCE} -DLOVE_JIT=0 -DCMAKE_BUILD_TYPE=Release -DCMAKE_POSITION_INDEPENDENT_CODE=ON -DLOVEJS_SINGLE=ON
+        emmake make -j 8 
+    )
 
-# (
-#   cd build/release-single
-#   emcmake cmake ${MEGASOURCE} -DLOVE_JIT=0 -DCMAKE_BUILD_TYPE=Release -DCMAKE_POSITION_INDEPENDENT_CODE=ON -DLOVEJS_SINGLE=ON
-#   emmake make -j 8 
-# )
+    (
+        cd build/compat
+        emcmake cmake ${MEGASOURCE} -DLOVE_JIT=0 -DCMAKE_BUILD_TYPE=Release -DLOVEJS_COMPAT=1 -DSEXPORT_ALL=1 -DSMAIN_MODULE=1 -DSERROR_ON_UNDEFINED_SYMBOLS=0 -DCMAKE_POSITION_INDEPENDENT_CODE=ON
+        emmake make -j 8
+    ) 
+    
+    (
+        cd build/compat-single
+        emcmake cmake ${MEGASOURCE} -DLOVE_JIT=0 -DCMAKE_BUILD_TYPE=Release -DLOVEJS_COMPAT=1 -DSEXPORT_ALL=1 -DSMAIN_MODULE=1 -DSERROR_ON_UNDEFINED_SYMBOLS=0 -DCMAKE_POSITION_INDEPENDENT_CODE=ON -DLOVEJS_SINGLE=ON
+        emmake make -j 8
+    )
+fi
+
+      ## Lua 5.4 build
+if [[ $lua54 = 1 ]]
+then
+      
+mkdir -p build/release-lua54 build/release-single-lua54 build/compat-lua54 build/compat-single-lua54
 
 (
-  cd build/compat
+  cd build/release-lua54
+  emcmake cmake ${MEGASOURCE} -DLOVE_JIT=0 -DLUA_54=1 -DCMAKE_BUILD_TYPE=Release -DCMAKE_POSITION_INDEPENDENT_CODE=ON
+  emmake make -j 8
+)
+
+(
+  cd build/release-single-lua54
+  emcmake cmake ${MEGASOURCE} -DLOVE_JIT=0 -DLUA_54=1 -DCMAKE_BUILD_TYPE=Release -DCMAKE_POSITION_INDEPENDENT_CODE=ON -DLOVEJS_SINGLE=ON
+  emmake make -j 8 
+)
+
+(
+  cd build/compat-lua54
   emcmake cmake ${MEGASOURCE} -DLOVE_JIT=0 -DLUA_54=1 -DCMAKE_BUILD_TYPE=Release -DLOVEJS_COMPAT=1 -DSEXPORT_ALL=1 -DSMAIN_MODULE=1 -DSERROR_ON_UNDEFINED_SYMBOLS=0 -DCMAKE_POSITION_INDEPENDENT_CODE=ON
   emmake make -j 8
 ) 
 
-# (
-#   cd build/compat-single
-#   emcmake cmake ${MEGASOURCE} -DLOVE_JIT=0 -DCMAKE_BUILD_TYPE=Release -DLOVEJS_COMPAT=1 -DSEXPORT_ALL=1 -DSMAIN_MODULE=1 -DSERROR_ON_UNDEFINED_SYMBOLS=0 -DCMAKE_POSITION_INDEPENDENT_CODE=ON -DLOVEJS_SINGLE=ON
-#   emmake make -j 8
-# ) 
+(
+  cd build/compat-single-lua54
+  emcmake cmake ${MEGASOURCE} -DLOVE_JIT=0 -DLUA_54=1 -DCMAKE_BUILD_TYPE=Release -DLOVEJS_COMPAT=1 -DSEXPORT_ALL=1 -DSMAIN_MODULE=1 -DSERROR_ON_UNDEFINED_SYMBOLS=0 -DCMAKE_POSITION_INDEPENDENT_CODE=ON -DLOVEJS_SINGLE=ON
+  emmake make -j 8
+) 
+fi
--- scripts/release.sh
+++ scripts/release.sh
@@ -5,7 +5,7 @@ rm release/*
 for file in $(ls build);
 do
     echo -e "building love$LOVE_VERSION-$file-$LOVEJS_VERSION.zip"
-    rm build/$file/love/love*.zip
+    ## rm build/$file/love/love*.zip
     ls build/$file/love/love* | grep -v ".zip" | env TZ=UTC zip -j -r -q -9 -X build/$file/love/love$LOVE_VERSION-$file-$LOVEJS_VERSION.zip -@
-    cp build/$file/love/love$LOVE_VERSION-$file-$LOVEJS_VERSION.zip release/
+    mv build/$file/love/love$LOVE_VERSION-$file-$LOVEJS_VERSION.zip release/
 done
