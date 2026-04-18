2bbb4f97b82926350fe0b445ef9c61987e3feb53
alexjgriffith <griffitaj@gmail.com>
2026-04-17 21:59:33 -0400

    prepared for beta2

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,9 +2,10 @@
 
 ## 3.1.68 is the sweet spot, same version of SDL
 ## 3.1.74 works (the version of SDL is newer than the one supported by Love11.5)
-## had issues with 4.0.8
+## had issues with 4.0.8, using SDL_free with regular malloc rather than SDL_malloc
+## 5.0.3 - more issues with SDL_free ...
 
-emsdk_version=4.0.0 # 3.1.68 #5.0.4
+emsdk_version=5.0.6 ## 4.0.0 # 3.1.68 #5.0.4
 
 git submodule init
 git submodule update
@@ -12,7 +13,7 @@ git submodule update
 ## Checkout the target branch of the love project
 cd love
 git checkout js-lua-interface
-cd ../
+cd ../make setup && make compile && cp /home/alexjgriffith/Github/lovejs-update/build/compat-single/love/love.js ~/.cache/lovejs/love115-compat-single-beta1/
 
 ## Checkout the target branch of the megasource project
 cd megasource
